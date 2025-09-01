46e8ab130908cbd6b189d23b5725808ac5a17fa5
Yagofs <yagofsarmento8@gmail.com>
2025-09-01 19:54:07 -0300

    Comentario adicionado

--- calcula_peso_ideal.py
+++ calcula_peso_ideal.py
@@ -1,5 +1,9 @@
+"""ESTE CÓDIGO REALIZA O CALCULO DO PESO IDEAL COM BASE NO SEXO DA PESSOA"""
+# Inserção dos dados
 altura = (float(input("Fornceça a Altura: ")))
 sexo = input("Forneça o Sexo (M/F)")
+# Verificação do sexo e cálculo do peso ideal
+# Caso não seja inserido o valor correto (M/F) retorna nulo.
 if sexo.upper() == "M":
     peso_ideal = (72.7 * altura) - 58
 elif sexo.upper() == "F":
